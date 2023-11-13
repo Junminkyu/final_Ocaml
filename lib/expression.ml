fc6488f78a4c29098a0fc1e35a8a061206fe9c85
jun00020 <jun00020@umn.edu>
2023-11-13 11:08:51 -0600

    Somewhat finished the 80% thingy

--- lib/expression.ml
+++ lib/expression.ml
@@ -6,7 +6,7 @@ module Lexer = Lexer
 let rec string_of_expression ( e : expression ) 
   = match e with 
   | Identifier nm -> nm
-  | Axiom nm -> nm
+  | Axiom nm -> "\n"^nm^"\n"
   | Prove nm -> nm
   | Application (e1,e2) -> (string_of_expression e1)^" "^
     (string_of_expression_with_parens e2)
@@ -14,7 +14,7 @@ let rec string_of_expression ( e : expression )
 and string_of_expression_with_parens e
   = match e with 
   | Identifier nm -> nm
-  | Axiom nm -> nm
+  | Axiom nm -> "\n"^nm^"\n"
   | Prove nm -> nm 
   | Application _ ->  "(" ^ string_of_expression e ^ ")"
   
